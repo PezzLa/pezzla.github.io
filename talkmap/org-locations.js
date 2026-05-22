2e8812b2ef9dde454c0ced4f669a12548bf5a045
Laura Pezzella <92517359+PezzLa@users.noreply.github.com>
2026-05-22 11:23:27 -0400

    Add files via upload

---
+++ talkmap/org-locations.js
@@ -0,0 +1,52 @@
+var addressPoints = [
+  [
+    "XVIII Black Holes Workshop<br />Instituto Superior Tecnico; Lisbon, PT",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "XVI ET Symposium<br />RWTH Aachen; Aachen, DE",
+    50.776351,
+    6.083862
+  ],
+  [
+    "Invited talk<br />University La Sapienza; Rome (IT)",
+    41.8933203,
+    12.4829321
+  ],
+  [
+    "Joint PRIN (A. Maselli, GSSI - D. Gerosa, Milano Biccoca) Meeting Theoretical Horizons in Unraveling Relativity, Astrophysics, and Mergers (THURAM) Meeting<br />Gran Sasso Science Institute; L'Aquila, IT",
+    42.1368853,
+    13.610341
+  ],
+  [
+    "Joint PRIN (A. Maselli, GSSI - D. Gerosa, Milano Biccoca) Meeting \textit{Linking Advances in our Understanding of Theoretical Astrophysics and Relativity to Observations (LAUTARO)}<br />University of Milano-Bicocca; Milan, IT",
+    45.4641943,
+    9.1896346
+  ],
+  [
+    "5th Meeting on GW Science in Scandinavia<br />Niels Bohr Institute; Copenhagen, DK",
+    55.6867243,
+    12.5700724
+  ],
+  [
+    "Joint MAECI (A. Maselli, GSSI - E. Berti, Johns Hopkins University) Meeting 1st GSSI-JHU MAECI meeting on Gravitational Wave Astrophysics<br />Gran Sasso Science Institute; L'Aquila, IT",
+    42.1368853,
+    13.610341
+  ],
+  [
+    "XV ET Symposium<br />Consiglio Nazionale delle Ricerche (CNR); Bologna, IT",
+    44.4938203,
+    11.3426327
+  ],
+  [
+    "24<sup>th</sup> International Conference on General Relativity and Gravitation & 16<sup>th</sup> Edoardo Amaldi Conference on Gravitational Waves<br />Scottish Exhibition and Conference Center; Glasgow, UK",
+    55.861155,
+    -4.2501687
+  ],
+  [
+    "Invited talk<br />Johns Hopkins University; Baltimore, MD, USA",
+    39.2908816,
+    -76.610759
+  ]
+];
